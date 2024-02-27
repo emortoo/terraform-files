b16e904662fefff659571f2a8df9e5ec0d2200f5
Emmanuel Mortoo <emortoo@yahoo.com>
2024-02-26 20:48:46 -0500

    new update for terrafoem file

--- terraform-file/provider.tf
+++ terraform-file/provider.tf
@@ -17,7 +17,7 @@ terraform {
 
 provider "aws" {
     region = var.region
-   // access_key = var.access_key
-   // secret_key = var.secret_key
+    access_key = var.access_key
+    secret_key = var.secret_key
   //  If you have entered your credentials in AWS CLI before, you do not need to use these arguments.
 }
